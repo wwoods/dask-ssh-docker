567f1d15c1e83933f984a752d265fa725780c0db
Walt Woods <waltw@galois.com>
2019-11-14 16:41:21 -0800

    Ability to clone 'docker login' auth for remote pulls

--- dask_ssh_docker/__init__.py
+++ dask_ssh_docker/__init__.py
@@ -1,4 +1,6 @@
+import copy
 import logging
+import os
 import sys
 from typing import List
 import warnings
@@ -26,6 +28,72 @@ def get_docker_cmd(image, global_config):
             '--env', 'DASK_INTERNAL_INHERIT_CONFIG="%s"' % serialize_for_cli(global_config),
             image,
     ]
+
+
+async def get_docker_image(connection, image):
+    """Some images in e.g. private repositories require use of the "docker
+    login" command before an image may be pulled.  However, we don't really
+    want to handle the user password.  So instead, grab the auth from the
+    current user, if it applies, and clean it up when done.
+    """
+    import asyncssh
+    docker_server = image.split('/', 1)[0]
+
+    import json
+    dat = json.load(open(os.path.expanduser('~/.docker/config.json')))
+    server_keys = dat.get('auths', {}).get(docker_server)
+    if server_keys is None:
+        # Nothing to do.
+        logger.info('docker_login=True specified, but current user has '
+                'no credentials at ' + docker_server)
+        return
+
+    # We want to be as non-destructive as possible.  So first verify that
+    # the target does not have a credential, and back up whatever credentials
+    # they do have.
+    old_json = None
+    try:
+        r = await connection.run('cat ~/.docker/config.json', check=True)
+    except asyncssh.ProcessError as e:
+        # No such file
+        pass
+    else:
+        old_json = json.loads(r.stdout)
+
+    if (old_json is not None
+            and old_json.get('auths', {}).get(docker_server, {}).get('auth')
+                == server_keys['auth']):
+        # Nothing to do, auths match already
+        return
+
+    # OK, need to add this server key, overwrite, pull, and restore file
+    if old_json is None:
+        new_json = {'auths': {docker_server: server_keys}}
+    else:
+        new_json = copy.deepcopy(old_json)
+        new_json.setdefault('auths', {})[docker_server] = server_keys
+
+    try:
+        await connection.run(f'mkdir -p ~/.docker', check=True)
+        await connection.run(f'cat > ~/.docker/config.json', check=True,
+                input=json.dumps(new_json))
+    except asyncssh.ProcessError as e:
+        logger.error(f'stderr: {e.stderr}')
+        raise e
+
+    try:
+        # Pull image
+        await connection.run(f'docker pull {image}',
+                check=True)
+    except asyncssh.ProcessError as e:
+        logger.error(f'stderr: {e.stderr}')
+        raise e
+    finally:
+        if old_json is not None:
+            await connection.run('cat > ~/.docker/config.json', check=True,
+                    input=json.dumps(old_json))
+        else:
+            await connection.run('rm ~/.docker/config.json', check=True)
 
 
 def get_docker_createprocess_kwargs():
@@ -91,6 +159,11 @@ class Worker(Process):
         The python module to run to start the worker.
     connect_options: dict
         kwargs to be passed to asyncssh connections
+    image: str
+        The docker image containing the required libraries
+    docker_login: bool
+        If True, specifies that the user executing dask_ssh_docker has Docker
+        credentials which should be used to pull images on the remote targets.
     kwargs: dict
         These will be passed through the dask-worker CLI to the
         dask.distributed.Worker class
@@ -102,6 +175,7 @@ class Worker(Process):
         address: str,
         connect_options: dict,
         image: str,
+        docker_login: bool,
         kwargs: dict,
         worker_module="distributed.cli.dask_worker",
         loop=None,
@@ -113,6 +187,7 @@ class Worker(Process):
         self.worker_module = worker_module
         self.connect_options = connect_options
         self.image = image
+        self.docker_login = docker_login
         self.kwargs = kwargs
         self.name = name
 
@@ -124,6 +199,8 @@ class Worker(Process):
         address, *opts = self.address.split(' ')
 
         self.connection = await asyncssh.connect(address, **self.connect_options)
+        if self.docker_login:
+            await get_docker_image(self.connection, self.image)
         self.proc = await self.connection.create_process(
                 " ".join(
                     get_docker_cmd(self.image, dask.config.global_config) + [
@@ -174,9 +251,10 @@ class Scheduler(Process):
     """
 
     def __init__(self, address: str, connect_options: dict, image: str,
-            kwargs: dict):
+            docker_login: bool, kwargs: dict):
         self.address = address
         self.image = image
+        self.docker_login = docker_login
         self.kwargs = kwargs
         self.connect_options = connect_options
 
@@ -191,6 +269,8 @@ class Scheduler(Process):
 
         self.connection = await asyncssh.connect(address, **self.connect_options)
 
+        if self.docker_login:
+            await get_docker_image(self.connection, self.image)
         worker_cmd = " ".join(
                 get_docker_cmd(self.image, dask.config.global_config) + [
                     "python",
@@ -222,6 +302,7 @@ class Scheduler(Process):
 def SSHDockerCluster(
     hosts: List[str] = ['localhost', 'localhost'],
     image: str = 'daskdev/dask:latest',
+    docker_login: bool = False,
     connect_options: dict = {},
     worker_module: str = "distributed.cli.dask_worker",
     **kwargs
@@ -253,6 +334,16 @@ def SSHDockerCluster(
         and 'localhost [--worker-opt value]' - that is, the address followed
         by a space-delimited list of arguments to send to dask-scheduler
         and dask-worker, respectively.
+    docker_login:
+        Some remote repositories, such as when image is specified as
+        ``'docker.repository.com:port/imagename:tag'``, require authentication.
+        If the ``docker_login`` flag is set, then the current user's
+        ~/.docker/config.json will be parsed for the given repository and
+        transferred to the remote machine so that it might pull down the
+        image.
+
+        This is disabled by default, because transferring auth without opting
+        in to that behavior would be concerning.
     connect_options:
         Keywords to pass through to asyncssh.connect
         known_hosts: List[str] or None
@@ -300,6 +391,7 @@ def SSHDockerCluster(
             "address": hosts[0],
             "connect_options": connect_options,
             "image": image,
+            "docker_login": docker_login,
             "kwargs": {},
         },
     }
@@ -310,6 +402,7 @@ def SSHDockerCluster(
                 "address": host,
                 "connect_options": connect_options,
                 "image": image,
+                "docker_login": docker_login,
                 "worker_module": worker_module,
                 "kwargs": {},
             },
